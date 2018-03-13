e24a7618d240748e7c65d65251fd7cb27d135cd4
Djim <oomesdjim@gmail.com>
2018-03-13 12:39:46 +0100

    commit5

---
+++ app/src/main/java/com/example/djim/marsrover/DetailScreen.java
@@ -0,0 +1,25 @@
+package com.example.djim.marsrover;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+/**
+ * Created by Djim on 13-3-2018.
+ */
+
+public class DetailScreen extends AppCompatActivity{
+
+    @Override
+    protected void onCreate(Bundle savedInstances){
+        super.onCreate(savedInstances);
+
+        Bundle extras = getIntent().getExtras();
+
+        ImageView image = findViewById(R.id.detailed_mars_image);
+        TextView textView = findViewById(R.id.detailed_mars_text);
+
+        textView.setText("Dummy text");
+    }
+}
